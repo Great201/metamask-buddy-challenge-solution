78c2b38bfd5c56b6d986402c24030492569dacbb
Great <greatoladipupo818@gmail.com>
2025-08-30 15:55:43 +0100

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,9 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+      <Analytics />
       </BrowserRouter>
     </TooltipProvider>
-    <Analytics />
   </QueryClientProvider>
 );
 
